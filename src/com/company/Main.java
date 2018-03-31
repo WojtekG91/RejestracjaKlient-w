8bfb58f9ca1565396cf49632a5bf198275d03d21
WojtekG91 <wojtek.galka91@gmail.com>
2018-03-31 17:25:05 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,21 @@
 package com.company;
 
-import com.company.Services.ServiceRegistration;
+import com.company.Services.Service;
+import com.company.Services.ServiceRepository;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Service service1 = new Service("Malowanie lakierem klasycznym", 50.00, 2.00);
+        Service service2 = new Service("Malowanie lakierem hybrydowym", 80.00, 1.50);
+        Service service3 = new Service("Żele", 100.00, 3.0);
+
+
+        ServiceRepository.showServices();
+//        System.out.println(service1.toString());
+//        System.out.println(service2.toString());
+//        System.out.println(service3.toString());
 
     }
 }
