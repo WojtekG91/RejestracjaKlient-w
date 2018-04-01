cc6d3aa6da37898f50568d54978a94a3ba8dbbc4
WojtekG91 <wojtek.galka91@gmail.com>
2018-04-01 12:14:54 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.Services.Service;
 import com.company.Services.ServiceRepository;
+import com.company.Users.Client;
 
 public class Main {
 
@@ -10,12 +11,20 @@ public class Main {
         Service service1 = new Service("Malowanie lakierem klasycznym", 50.00, 2.00);
         Service service2 = new Service("Malowanie lakierem hybrydowym", 80.00, 1.50);
         Service service3 = new Service("Żele", 100.00, 3.0);
+        Service service4 = new Service("Żele 21", 110.00, 3.0);
 
+        System.out.println("---------------------------");
+        ServiceRepository.addService(service1);
+        ServiceRepository.addService(service2);
+        ServiceRepository.addService(service3);
+        ServiceRepository.addService(service4);
 
         ServiceRepository.showServices();
-//        System.out.println(service1.toString());
-//        System.out.println(service2.toString());
-//        System.out.println(service3.toString());
+
+
+
+        Client Ewelina = new Client("Ewelina", "Sanka","66666666");
+        Ewelina.chooseService(1);
 
     }
 }
