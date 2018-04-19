df1b4370b05f008794bfd5b58664019d5b8a8e33
WojtekG91 <wojtek.galka91@gmail.com>
2018-04-19 22:11:54 +0200

    DB connected

--- src/main/java/com/company/Users/Login.java
+++ src/main/java/com/company/Users/Login.java
@@ -4,14 +4,20 @@ import com.company.SQL.SQLStatements;
 
 public class Login {
     private String mail, password;
-    Hashing hashing = new Hashing();
-    public void LoginAtempt(String mail, String password){
-        String dbPassword = SQLStatements.loginStatement(mail);
-        password=hashing.HashingPassword(password);
-        if (dbPassword.equals(password)){
-            System.out.println("Logowanie udane");
-        }else {
-            System.out.println("Niepoprawny email lub hasło");
+    private Hashing hashing = new Hashing();
+
+    public void LoginAtempt(String mail, String password) {
+        String dbPassword = SQLStatements.getSqlPassword(mail);
+        boolean mailCheck = SQLStatements.getSqlMail(mail);
+        if (mailCheck) {
+            password = hashing.HashingPassword(password);
+            if (dbPassword.equals(password)) {
+                System.out.println("Logowanie udane");
+            } else {
+                System.out.println("Niepoprawny email lub hasło");
+            }
+        } else {
+            System.out.println("no user with this mail");
         }
     }
 }
